44c95d6112d92e935b7fee34f578fb4aec3ae2a2
Fede Antonelli <antonelli.fd@gmail.com>
2022-12-02 22:48:03 -0300

    REFACTOR: dtos added for modules

--- src/interfaces/cart.interface.ts
+++ src/interfaces/cart.interface.ts
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose';
 
-export interface ItemCart {
-  _id: mongoose.Types.ObjectId;
-  quantity: number;
-  total: number;
-}
+import { CartDTO } from '../api/cart/cart.dto';
 
 export interface ItemCartDocument extends mongoose.Document {
   _id: mongoose.Types.ObjectId;
@@ -14,13 +10,7 @@ export interface ItemCartDocument extends mongoose.Document {
   updateAt: Date;
 }
 
-export interface Cart {
-  userId: mongoose.Types.ObjectId;
-  products: ItemCart[];
-  total: number;
-}
-
-export interface CartDocument extends Cart, mongoose.Document {
+export interface CartDocument extends CartDTO, mongoose.Document {
   createdAt: Date;
   updateAt: Date;
 }
